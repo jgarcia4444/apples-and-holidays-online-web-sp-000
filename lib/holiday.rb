2f07c52ff602c1bf9c992203571e33586978abef
Jake Garcia <bballwiz4@gmail.com>
2020-02-26 01:37:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -110,7 +110,7 @@ def all_holidays_with_bbq(holiday_hash)
   bbq_holidays = []
   holiday_hash.each do |season, data|
     data.each do |holiday, supplies|
-      if supplies.includes?("BBQ")
+      if supplies.include?("BBQ")
         bbq_holidays << holiday
       end
     end
