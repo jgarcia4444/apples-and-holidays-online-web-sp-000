3f623c62ac01c066082022f5846ed252c0c45f59
Jake Garcia <bballwiz4@gmail.com>
2020-02-26 01:29:39 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -107,5 +107,9 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  holiday_hash.select do |season, holiday|
+    if season[holiday].include?("BBQ")
+      holiday
+    end
+  end
 end
