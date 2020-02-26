702c1367854faeec5fe7ffad2b0937c107e3424a
Jake Garcia <bballwiz4@gmail.com>
2020-02-26 01:06:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,7 +50,7 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  holiday_hash[:winter].collect do |key, value|
+  holiday_hash[:winter].each do |key, value|
     value.flatten
   end
 end
